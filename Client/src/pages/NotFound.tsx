810a1c8a9af3469853197a2850cff184d84bc24d
taha harbouch <tahaharbouch1@gmail.com>
2023-06-05 17:17:44 +0100

    update

---
+++ Client/src/pages/NotFound.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+const NotFoud = () => {
+    return (
+        <section>
+            <div className="not-found" >
+                <h1>Oops!</h1>
+                <p>Page Not Found</p>
+                <div>
+                    <img src="/images/error404.jpg" alt="" />
+                </div>
+                <div >
+                    <Link to="/">Visit Our Homepage</Link>
+                </div>
+            </div>
+        </section>
+
+    )
+}
+
+export default NotFoud
