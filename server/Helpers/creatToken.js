20eacf05d499ab22d83dcf6ee03efe435f56f31f
taha harbouch <tahaharbouch1@gmail.com>
2023-06-08 10:07:50 +0100

    update

--- server/Helpers/creatToken.js
+++ server/Helpers/creatToken.js
@@ -1,5 +1,11 @@
 const jwt = require("jsonwebtoken");
 
-module.exports.createSecretToken = (username, fullname, userID) => {
+module.exports.createSecretToken = (email,username, fullname, userID,condition) => {
+    if (condition) {
+        
     return jwt.sign({ username, fullname, userID }, process.env.TOKEN_SECRET);
+    }
+    else{
+        return jwt.sign({email}, process.env.TOKEN_SECRET,{ expiresIn: '15m' });
+    }
 };
