a79da6705ba315128e997615cb9753e6b14e4129
Tharbouch <tahaharbouch1@gmail.com>
2024-12-16 17:24:12 +0100

    add route

--- server/Routes/testingRoute.js
+++ server/Routes/testingRoute.js
@@ -1,6 +1,6 @@
-const route = require('express').Router()
-const accessibilityCheck = require('../Controllers/checkAccessibilty/test/checkAccessibility')
-const getReport = require('../Controllers/checkAccessibilty/getReport/reportController')
+const route = require('express').Router();
+const accessibilityCheck = require('../Controllers/checkAccessibilty/test/checkAccessibility');
+const getReport = require('../Controllers/checkAccessibilty/getReport/reportController');
 
 route.get('/getReport', getReport)
 route.post('/check-accessibility', accessibilityCheck);
--- server/Routes/user.js
+++ server/Routes/user.js
@@ -1,11 +1,12 @@
-const route = require('express').Router()
-const controller = require('../Controllers/user/userController')
+const route = require('express').Router();
+const controller = require('../Controllers/user/userController');
 
 
-route.post('/login', controller.logUser)
-route.post('/register', controller.registerUser)
-route.get('/loggedIn', controller.checkLogIn)
-route.post('/logout', controller.logout)
+route.post('/login', controller.logUser);
+route.post('/register', controller.registerUser);
+route.get('/loggedIn', controller.checkLogIn);
+router.get('/refresh',controller.refreshToken);
+route.post('/logout', controller.logout);
 // route.post('/password-recovery', controller.passwordRecovery) will be fixed later
 
 module.exports = route
